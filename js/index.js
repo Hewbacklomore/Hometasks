ee5af63c269a9d5bf27e4c3953c545e681498f1e
Artur Peiev <fromtheisland13@gmail.com>
2023-03-10 16:36:42 +0000

    third hometask

--- js/index.js
+++ js/index.js
@@ -1,32 +1,53 @@
 'use strict';
 
-// #TASK 2
+// #TASK 3
 
-const askUserHours = +prompt('Write number of hours', ''); 
+// P.S I`m not sure that I understand right the task but anyway I made it by my way.
 
+ // 1) 
+ let num = 1;
 
-
-const oneHourInSeconds = 60;
-
-let hours = 0;
-
-if(askUserHours === 'string' && askUserHours === 'undefined' && askUserHours === 'null') {
-    alert('Bro, c`mon write numbers!')
-}
-
-let arrHours = [askUserHours];
-for(let i = 0; i < arrHours.length; i++) {
-    hours += arrHours[i] * oneHourInSeconds;
-    alert(hours);
-}
+console.log(num += 1) // num + 1; num = 2;
+console.log(num -= 1); // num - 1;  num = 1;
+console.log(num *= 1) // num * 1; num = 1;
+console.log(num /= 1) // num / 1; num = 1;
+console.log(++num)  /// num = 2;
+console.log(--num); // num = 1;
 
 
-// Ну и второй метод через if либо можно было попробоавать switch, но больше всего мне понарвилось через массив;
 
-/* if(askUserHours == 2) {
-    hours = askUserHours * oneHourInSeconds
-    alert(hours)
-}else if(askUserHours == 3) {
-    hours = askUserHours * oneHourInSeconds
-}........  и
- */
+
+
+
+// 2) Тут мы должны получить  num = num + 12
+let num1 = 1;
+ 
+console.log(num1 += 12); // num1 + 12; num1 = 13;
+console.log(num1 -= 12); // num1 - 12; num1 = 1;
+console.log(num1 *= 12); // num1 * 12; num1 = 12;
+console.log(num1 /= 12); // num1 + 12; num1 = 1;
+console.log(++num1); // num1 = 2;
+console.log(--num1);// num1 = 1;
+
+
+//3) num = num — 14;
+let num2 = 16;
+
+console.log(num2 += 14) // num2 + 14; num2 = 30;
+console.log(num2 -= 14) // num2 - 14; num2 = 16;
+console.log(num2 *= 14) // num2 * 14; num2 = 224;
+console.log(++num2) //  num2 = 225;
+console.log(--num2) //  num2 = 224;
+console.log(num2 / 14) // num2 / 14; num2 = 16;
+
+
+//4) num = num * 5;
+
+let num3 = 3;
+
+console.log(num3 *= 5) // num3 * 5; num3 = 15;
+console.log(num3 /= 5) // num3 / 5; num3 = 3;
+console.log(num3 += 5) // num3 + 5; num3 = 8;
+console.log(num3 -= 5) // num3 - 5; num3 = 3;
+console.log(++num3) // num3 = 4;
+console.log(--num3) // num3 = 3;
