d3eb9eec8eb0224d273279465c177c347fe57d81
Artur Peiev <fromtheisland13@gmail.com>
2023-03-13 12:35:20 +0000

    fourth commit

--- js/index.js
+++ js/index.js
@@ -1,53 +1,28 @@
 'use strict';
 
-// #TASK 3
-
-// P.S I`m not sure that I understand right the task but anyway I made it by my way.
-
- // 1) 
- let num = 1;
-
-console.log(num += 1) // num + 1; num = 2;
-console.log(num -= 1); // num - 1;  num = 1;
-console.log(num *= 1) // num * 1; num = 1;
-console.log(num /= 1) // num / 1; num = 1;
-console.log(++num)  /// num = 2;
-console.log(--num); // num = 1;
+// #TASK 4
 
 
+const a = +prompt('Write first number', '');
+const b = +prompt('Write second number', '');
+
+function equal (a, b) {
+    return a === b
+}
+
+console.log(equal(a, b));
+
+function sum (a, b) {
+    return a + b > 10
+}
+
+console.log(sum(a, b));
 
 
+const c = +prompt('Write thrid number', '');
 
+function negativeNumber (c) {
+    return c < 0
+}
 
-// 2) Тут мы должны получить  num = num + 12
-let num1 = 1;
- 
-console.log(num1 += 12); // num1 + 12; num1 = 13;
-console.log(num1 -= 12); // num1 - 12; num1 = 1;
-console.log(num1 *= 12); // num1 * 12; num1 = 12;
-console.log(num1 /= 12); // num1 + 12; num1 = 1;
-console.log(++num1); // num1 = 2;
-console.log(--num1);// num1 = 1;
-
-
-//3) num = num — 14;
-let num2 = 16;
-
-console.log(num2 += 14) // num2 + 14; num2 = 30;
-console.log(num2 -= 14) // num2 - 14; num2 = 16;
-console.log(num2 *= 14) // num2 * 14; num2 = 224;
-console.log(++num2) //  num2 = 225;
-console.log(--num2) //  num2 = 224;
-console.log(num2 / 14) // num2 / 14; num2 = 16;
-
-
-//4) num = num * 5;
-
-let num3 = 3;
-
-console.log(num3 *= 5) // num3 * 5; num3 = 15;
-console.log(num3 /= 5) // num3 / 5; num3 = 3;
-console.log(num3 += 5) // num3 + 5; num3 = 8;
-console.log(num3 -= 5) // num3 - 5; num3 = 3;
-console.log(++num3) // num3 = 4;
-console.log(--num3) // num3 = 3;
+console.log(negativeNumber(c))
