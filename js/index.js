d1aae2bb73e17b4769c9161c37b1458570b30c81
Artur Peiev <fromtheisland13@gmail.com>
2023-03-14 17:11:48 +0000

    seventh commit

--- js/index.js
+++ js/index.js
@@ -2,25 +2,51 @@
 
 // #TASK 7
 
-//Вам нужно запросить у пользователя число. Добавить условие при котором число будет выводиться (через alert) с правильным словом: «год», «года» или «лет». Т.е. «29 лет» или «4 года».
+//При загрузке страницы нужно показать пользователю окно, в котором спросить его год рождения.
+//Когда он введет его и нажмет ОК, спросить у него, в каком городе он живет.
+//При удачном раскладе показать ему еще одно окно, где спросить его любимый вид спорта.
+//При нажатии на ОК показываем ему окно, где должна быть отображена следующая информация:
+//Его возраст.
+//Если пользователь введет Киев, Москва или Минск, то показать ему сообщение – “Ты живешь в столице …” и на место точек подставляем страну, столицу которой он ввел. Иначе показываем ему “ты живешь в городе …” и на место точек – введенный город.
+//Выбираем сами три вида спорта и три чемпиона в этих видах. Соответственно, если пользователь введет один из этих видов спорта, то показываем ему сообщение “Круто! Хочешь стать …” и подставляем на место точек имя и фамилию чемпиона.
+ 
 
-//Ограничений по возрасту — нет, 0 тоже подходит.
+//Всё это должно быть отображено в одном окне.
 
-//Также сделайте простую проверку, что пользователь ввел число.
+//Если в каком-то из случаев он не захочет вводить информацию и нажмет Отмена, показать ему сообщение – “Жаль, что Вы не захотели ввести свой(ю) …” и указываем, что он не захотел вводить – дату рождения, город или вид спорта.
 
-let askUser = +prompt('Write a number');
 
-switch(askUser) {
-        case isNaN(askUser):
-            alert('Write numbers');
-            break;
-        case askUser = 0:
-            alert(`${askUser} лет`);
-            break;
-        case askUser = 1:
-            alert(`${askUser} год`);
-            break;
-        case (askUser = 2):
-            alert(`${askUser} года`)
-            break;
+const dateOfBirth = +prompt('Write down year of your birth', '');
+
+
+if(dateOfBirth !== null && dateOfBirth !== 0) {
+   const currentCity = prompt('Which city do u live?', '');
+    if(currentCity !== null && currentCity !== '') {
+        const favouriteSport = prompt('What is favorite kind of sport?' , '')
+        if(favouriteSport !== null && favouriteSport !== '') {
+            alert(dateOfBirth)
+            if(favouriteSport === 'football') {
+                alert('Thats cool! Wanna be like Ronaldo?')
+            }else if(favouriteSport === 'boxing') {
+                alert('Thats cool! Wanna be like Klichko?')
+            }else if(favouriteSport === 'tennis') {
+                alert('Thats cool! Wanna be like Federer?')
+            }
+    }else {
+        alert('Its a pitу that u didnt write your favourite kind of sport')
+    } 
+         if(currentCity === 'Kiyv') {
+            alert(`Youre living in capital of Ukraine`)
+        }else if(currentCity === 'moskwa') {
+            alert('You are living in morder')
+        }else if(currentCity === 'Minsk') {
+            alert('You are living in Belarus')
+        }else {
+            alert(`You are living in ${currentCity}`)
+        } 
+    }else {
+        alert('Its a pitу that u didnt write your city')
+    }
+}else {
+    alert('Its a pitу that u didnt write your date of birth')
 }
