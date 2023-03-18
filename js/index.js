1d1eb87fe3c8caae8e7124e01031666fc1a49473
Artur Peiev <fromtheisland13@gmail.com>
2023-03-18 15:49:16 +0000

    task_11

--- js/index.js
+++ js/index.js
@@ -1,31 +1,16 @@
 'use strict';
 
-// #TASK 10
+// #TASK 11
 
 
-// Вам нужно запросить у пользователя число. Добавить условие при котором число будет выводиться (через alert) с правильным словом: «год», «года» или «лет». Т.е. «29 лет» или «4 года».
-
-//Ограничений по возрасту — нет, 0 тоже подходит.
-
-//Также сделайте простую проверку, что пользователь ввел число.
-
+const x = 4;
+const y = 9;
+const r = 10;
+const distance = Math.sqrt(x * x + y * y);
 
 
+distance <= r ?  alert('The point inside the circle') : alert('The point is out of circle')
 
-const age = +prompt("How old r u", '');
 
-if(!isNaN(age) && age !== '') {
-    if(age >= 5 && age <= 20) {
-      alert(`${age} лет`)
-    }else if(age % 10 === 1) {
-      alert(`${age} год`)
-    }else if(age % 10 >= 2 && age % 10 <= 4 ) {
-      alert(`${age} годa`)
-    }else {
-      alert(`${age} лет`)
-    }
-}else {
-  alert('Try one more time')
-}
 
 
