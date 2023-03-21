3328bd8e682cc71db0500012770a60b59ecadeef
Artur Peiev <fromtheisland13@gmail.com>
2023-03-21 16:44:23 +0000

    Task-12

--- js/index.js
+++ js/index.js
@@ -1,16 +1,34 @@
 'use strict';
 
-// #TASK 11
+// #TASK 12
+/* 
+Нужно создать функцию которая будет возводить число в степень
+
+Запрашиваем у пользователя число (с помощью prompt)
+Запрашиваем у пользователя степень, в которую это число нужно возвести (с помощью prompt)
+Создаем функцию которая принимает 2 аргумента (число, степень)
+Задаем аргументу который принимает степень значение по умолчанию 1
+Внутри функции нужно написать проверку, если аргументы не являются числами завершить функцию с указанием того, что пользователь ввел неправильные данные, например (return ‘some error’)
+ Если проверка прошла успешно, то возвести число в степень (работаем с аргументами функции)
+Вернуть результат выполнения функции и завершить ее
+Результат вызова функции записать в переменную и вывести пользователю через alert()
+ */
 
 
-const x = 4;
-const y = 9;
-const r = 10;
-const distance = Math.sqrt(x * x + y * y);
 
+const num = +prompt('Write frist number', '');
+const exponent = +prompt('Write second number', ''); 
 
-distance <= r ?  alert('The point inside the circle') : alert('The point is out of circle')
+function mult(firstNum, secondNum = 1) { 
+  console.log(secondNum)
+  if(isNaN(firstNum) && isNaN(secondNum)) {  // if(type of firstNum !== 'number && typeof secondNum !== 'number')
+    return 'some error'
+  }else { 
+    return Math.pow(firstNum, secondNum)
+  }
+}
 
-
+let result = mult(num, exponent); 
+alert(result); 
 
 
