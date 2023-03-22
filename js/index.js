f5405422f85c3dfe6cd698249770565d1faa26ed
Artur Peiev <fromtheisland13@gmail.com>
2023-03-22 20:15:51 +0000

    Task_12

--- js/index.js
+++ js/index.js
@@ -1,18 +1,7 @@
 'use strict';
 
 // #TASK 12
-/* 
-Нужно создать функцию которая будет возводить число в степень
 
-Запрашиваем у пользователя число (с помощью prompt)
-Запрашиваем у пользователя степень, в которую это число нужно возвести (с помощью prompt)
-Создаем функцию которая принимает 2 аргумента (число, степень)
-Задаем аргументу который принимает степень значение по умолчанию 1
-Внутри функции нужно написать проверку, если аргументы не являются числами завершить функцию с указанием того, что пользователь ввел неправильные данные, например (return ‘some error’)
- Если проверка прошла успешно, то возвести число в степень (работаем с аргументами функции)
-Вернуть результат выполнения функции и завершить ее
-Результат вызова функции записать в переменную и вывести пользователю через alert()
- */
 
 
 
@@ -20,12 +9,15 @@ const num = +prompt('Write frist number', '');
 const exponent = +prompt('Write second number', ''); 
 
 function mult(firstNum, secondNum = 1) { 
+  
+if(num === null || exponent === exponent) return 'some error'
+
    
-  if(isNaN(firstNum) || isNaN(secondNum)) {  // if(type of firstNum !== 'number || typeof secondNum !== 'number')
+  if(isNaN(firstNum) || isNaN(secondNum)) {  
     return 'some error'
-  }else { 
+  } 
     return Math.pow(firstNum, secondNum)
-  }
+  
 }
 
 let result = mult(num, exponent); 
