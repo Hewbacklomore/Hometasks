a915a01f178f4e66e80b09527f9cf683c6515575
Artur Peiev <fromtheisland13@gmail.com>
2023-03-22 16:13:59 +0000

    Task-14

--- js/index.js
+++ js/index.js
@@ -1,53 +1,56 @@
 'use strict';
 
-// #TASK 13
+// #TASK 14
+
 /* 
-Вам нужно написать реализацию функции padString, которая принимает 4 аргумента:
+Дан массив с числами const arr = [1, 2, 3, -1, -2, -3];
 
-- строку
-- число, которое является длинной строки, которую мы хотим получить в результате выполнения функции
-- символ (строка длинной 1 символ) — которым дополнится строка, если это будет необходимо
-- параметр булеан (true или false), который определяет, добавлять символы слева или справа (по умолчанию справа)
-Обязательно при написании функции необходимо проверить аргументы, которые мы передали, и если каких-то аргументов нет, то вернуть из функции строку с ошибкой (return ‘some error’). Сообщение с ошибкой должно быть разное в зависимости от того условия, по которому не прошла проверка.
+Нкобходимо написать функцию которая вернет новый массив содержащий только положиельные числа. Для этого:
 
-Результат вызова функции нужно вывести в консоль — после завершения функции.
+— Создаем функцию с произвольным именем
 
-Например:
+— Функция принимает массив в качестве параметра
 
-Вызов padString(‘hello’, 8, ‘*’) вернет строку hello***
+— В теле функции создаем пустой массив (пример: const exampleArr = [])
 
-А вызов padString(‘hello’, 6, ‘*’, false) вернет строку *hello
+— Если массив переданный в параметр функции пустой, то возвращаем сообщение об этом
 
-Вызов padString(‘hello’, 2) вернет ‘he’ — если число меньше, чем размер введенной строки, нужно строку обрезать при помощи метода substr
+— Перебираем циклом массив, который передан в качестве параметра (использовать цикл for)
+
+— В теле цикла проверям является ли текущий элемент положительным числом или отрицательным (if)
+
+— Если число положительное, то добавлем его в ранее созданный массив с помощь функции push (пример exampleArr.push(currentPositiveNumber))
+
+— После выполнения цикла проверям массив, который наполняли только положиельными значениями
+
+— Если он не пустой, то возвращаем этот массив
+
+— Если пустой, возвращаем null
  */
 
-function padString (string, num, sym, bool = false) {
 
-      if(string == '' || typeof(string) !== 'string') {
-        return 'write string'
-      }else if(isNaN(num) || num < 0) {
-        return 'write number or bigger than 0'
-      }else if(num < string.length) {
-        return string.substring(0, num)
-      }else if(num > string.length) {
-        
-        if(bool) {
-          if(num - string.length === 3) {
-            return `${string}${sym}${sym}${sym}`
-         }else if(num - string.length === 1) {
-          return `${sym}${string}`
-         } 
-        }else {
-          if(num - string.length === 1) {
-            return `${sym}${string}`
-         }else if(num - string.length === 3) {
-          return `${sym}${sym}${sym}${string}`
-         } 
-        }
-      }
+const arr = [1, 2, 3, -1, -2, -3];
 
+function createNewPositiveArr(item) {
+ 
+    var exampleArr = [];
+
+    if(!item.length) {
+        return 'arr is empthy'
+    }else {
+         for(let i = 0; i < item.length; i++) {
+           if(item[i] < 0) {
+              console.log(`${item[i]} - negative numbers`)
+           }else if(item[i] > 0) {
+            let currentPositiveNumber = item[i];
+                exampleArr.push(currentPositiveNumber)
+           }
+        } 
+    }
+    return exampleArr
 }
 
-console.log(padString('hello', 8, '*')) 
-console.log(padString('hello', 2)) 
-console.log(padString('hello', 6, '*', false)) 
+
+console.log(createNewPositiveArr(arr))
+
+
