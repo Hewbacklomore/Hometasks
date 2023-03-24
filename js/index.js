f47216040698e0045ea26345b53eac6435f110b9
Artur Peiev <fromtheisland13@gmail.com>
2023-03-24 16:52:09 +0000

    Task-13

--- js/index.js
+++ js/index.js
@@ -3,12 +3,30 @@
 // #TASK 13
 
 
-function padString (string, num, sym, bool = false) {
+function padString (string, num, sym, bool = true) {
 
-     if(typeof string !== string)  throw new Error('Write string')
-     if(typeof num !== 'number' || isNaN(num) ) throw new Error('Write a number')
+     if(typeof string !== 'string')  throw new Error('Write string')
+      if(typeof num !== 'number' || Number.isNaN(num) || !isFinite(num)) {
+          return ('Write a number')
+     }  
+
+
+     if(string === num) return string
+     if(string.length > num) return string.substring(0, num)
+     
+
+     if(typeof sym !== 'string' || sym.length !== 1) throw new Error('Sym should be with one symbol with 1 length')
+
+     if(typeof bool !== 'boolean') throw new Error('bool should be Boolean');
+     
+     
+
+
+     const addSymbol = sym.repeat(string.length - length)
+
+     return bool ? string + addSymbol : addSymbol + string
 }
 
 console.log(padString('hello', 8, '*')) 
 console.log(padString('hello', 2)) 
-console.log(padString('hello', 6, '*', false)) 
+console.log(padString('hello', 6, '*', false))  
