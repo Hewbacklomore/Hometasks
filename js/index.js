b4f5270c92db9d8a27d9ce8e363e10809c5b51d5
Artur Peiev <fromtheisland13@gmail.com>
2023-03-28 12:44:45 +0100

    Task_19

--- js/index.js
+++ js/index.js
@@ -3,53 +3,45 @@
 // #Task 19
 
 
-
-function mainFunction(callback, mult, div, rules) {
+function mainFunction(callback, mult, div) { 
  
-  const num1 = +prompt('Write first number', '');
-  const num2 = +prompt('Write second number', ''); 
+  const num1 = +prompt('Write first number', ''); 
+  const num2 = +prompt('Write second number', '');  
   
-  rules(num1, num2)
-  callback(num1, num2);
-  mult(num1, num2)
-  div(num1, num2)
+  
+  callback(num1, num2); 
+  mult(num1, num2); 
+  div(num1, num2);
+  modullo(num1, num2);
   
 }
 
-const exponentiation = (a, b) => {
-  mainRules()
+const exponentiation = (a, b,) => { 
+
 
   const result = Math.pow(a, b)
   return alert(`The result of pow: ${result}`)
 }
 
-const multiplay = (a, b) => {
-  mainRules()
+const multiplay = (a, b) => { 
 
   const result = Math.round(a * b)
   return alert(`The result of multiplay: ${result}`)
-  
 }
 
-const divsion = (a, b) => {
-  mainRules()
+const divsion = (a, b) => { 
 
-  const result = Math.round(a / b)
+  const result = Math.round(a / b); 
   return alert(`The result of division: ${result}`)
-
 }
 
-const modullo = (a, b) => {
-  mainRules()
-  
+const modullo = (a, b) => { 
+
   const result = Math.round(a % b)
   return alert(`The result modullo: ${result}`)
 }
 
-const mainRules = (a, b) => {
-    if(isNaN(a) || isNaN(b) || typeof a !== 'number' || typeof b !== 'number') throw new Error('Write a number')
-        
-} 
 
 
-mainFunction(exponentiation, multiplay, divsion, modullo, mainRules)
+mainFunction(exponentiation, multiplay, divsion, modullo);  
+
