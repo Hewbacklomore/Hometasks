159cd09ec252adb169561762e31c5353dc864681
Artur Peiev <fromtheisland13@gmail.com>
2023-04-19 23:37:55 +0100

    Task-30

--- js/index.js
+++ js/index.js
@@ -1,3 +1,38 @@
 'use strict';
 
 // #Task 30
+
+
+function anotherSum (num) {
+
+    function plusSum () {
+        num = num + num
+        return num
+    }
+
+    return plusSum 
+}
+
+
+const sum = anotherSum(3);
+console.log(sum)
+
+
+ console.log(sum(3))
+ console.log(sum(5))
+console.log(sum(20))
+
+
+
+function counter (num = 1) {
+
+    function increase () {
+        num = num + 1;
+        return num
+    }
+
+    return increase
+}
+
+const countIncrease = counter(10);
+console.log(countIncrease);
