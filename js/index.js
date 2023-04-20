67e20de32c31c399dc0923c5d8f962535de2a20e
Artur Peiev <fromtheisland13@gmail.com>
2023-04-20 22:23:25 +0100

    Task-32

--- js/index.js
+++ js/index.js
@@ -1,22 +1,57 @@
 'use strict';
 
-// #Task 31
+// #Task 32
 
 
-(function () {
+(function() {
 
-    function counter (num = 0) {
+function counter(num) {
+    let newCounterObj = {};
+    let countIncrease = 0;
+    let countDecrease = 0;
+    let countValue = 0;
 
-        function changeCounter (x) {
-            num += x
-            return num
+
+    function createCounter() {
+         return newCounterObj = {
+            increase: function() {
+                num = num + 1
+                return `Function increase has been called: ${countIncrease++} and resuslt function increase: ${num}`
+            },
+            decrease: function() {
+                num = num - 1
+                return `Function decrease has been called: ${countDecrease++} and resuslt function decraese: ${num}`
+            },
+            value: function() {
+                return `Function value has been called: ${countValue++} and resuslt function value: ${num}`
+            },
+            getStatic: function() {
+                return {
+                    increase: countIncrease,
+                    decrease: countDecrease,
+                    value: countValue
+                }
+            },
+            reset: function() {
+                return countDecrease = 0,
+                       countDecrease = 0,
+                       countValue = 0;
+            }
         }
-            return changeCounter
     }
+    return createCounter()
+    
+}
 
-    const sum = counter();
-    sum(3);
-    sum(5);
-    sum(20);
+const myCounter1 = counter(1);
 
-} ())
+
+console.log(myCounter1.increase());
+console.log(myCounter1.increase());
+console.log(myCounter1.increase());
+console.log(myCounter1.decrease());
+console.log(myCounter1.getStatic());
+
+
+}()) 
+ 
