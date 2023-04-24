d6cccb6700416db1a13250d6e1e45d86eabf17b1
Artur Peiev <fromtheisland13@gmail.com>
2023-04-24 18:09:41 +0100

    Task-33

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@
 // #Task 33
 
 
+
+
 const calculator = {
     
     read() {
@@ -10,21 +12,24 @@ const calculator = {
       this.a = +prompt('Write your first number', '');
       this.b = +prompt('Write your second number', '');
 
-      if(this.a === 'string' || isNaN(this.a) || this.b === 'string' || isNaN(this.b)) return 'error' 
+      if(isNaN(this.a) || this.a === false || isNaN(this.b) || this.b === false) return 'error' 
 
       return this
        
     },
     sum() {
+
         return this.a + this.b
     },
     mult() {
+    
         return this.a * this.b
     }
+    
 
 }
 
 console.log(calculator.read());
-alert( calculator.sum());
-alert( calculator.mult()); 
+alert(calculator.sum());
+alert(calculator.mult()); 
 
