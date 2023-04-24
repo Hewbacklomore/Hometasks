266776a7ed0962f639fea10f82269cc6da9ade53
Artur Peiev <fromtheisland13@gmail.com>
2023-04-24 17:44:55 +0100

    Task-33

--- js/index.js
+++ js/index.js
@@ -1,57 +1,30 @@
 'use strict';
 
-// #Task 32
+// #Task 33
 
 
-(function() {
-
-function counter(num) {
-    let newCounterObj = {};
-    let countIncrease = 0;
-    let countDecrease = 0;
-    let countValue = 0;
-
-
-    function createCounter() {
-         return newCounterObj = {
-            increase: function() {
-                num = num + 1
-                return `Function increase has been called: ${countIncrease++} and resuslt function increase: ${num}`
-            },
-            decrease: function() {
-                num = num - 1
-                return `Function decrease has been called: ${countDecrease++} and resuslt function decraese: ${num}`
-            },
-            value: function() {
-                return `Function value has been called: ${countValue++} and resuslt function value: ${num}`
-            },
-            getStatic: function() {
-                return {
-                    increase: countIncrease,
-                    decrease: countDecrease,
-                    value: countValue
-                }
-            },
-            reset: function() {
-                return countDecrease = 0,
-                       countDecrease = 0,
-                       countValue = 0;
-            }
-        }
-    }
-    return createCounter()
+const calculator = {
     
+    read() {
+
+      this.a = +prompt('Write your first number', '');
+      this.b = +prompt('Write your second number', '');
+
+      if(this.a === 'string' || isNaN(this.a) || this.b === 'string' || isNaN(this.b)) return 'error' 
+
+      return this
+       
+    },
+    sum() {
+        return this.a + this.b
+    },
+    mult() {
+        return this.a * this.b
+    }
+
 }
 
-const myCounter1 = counter(1);
+console.log(calculator.read());
+alert( calculator.sum());
+alert( calculator.mult()); 
 
-
-console.log(myCounter1.increase());
-console.log(myCounter1.increase());
-console.log(myCounter1.increase());
-console.log(myCounter1.decrease());
-console.log(myCounter1.getStatic());
-
-
-}()) 
- 
