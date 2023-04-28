05f0d8574b8f48b23a4ac82394cb73b1d44fa9bf
Artur Peiev <fromtheisland13@gmail.com>
2023-04-28 15:32:29 +0100

    Task-36

--- js/index.js
+++ js/index.js
@@ -1,32 +1,3 @@
 'use strict';
 
-// #Task 35
-/* 
-function getFactorial(num) {
-  
-  function factorial() {
-      if(num) num * ()
-  }
-
-   
-    
-};
-
-console.log(getFactorial(3));
- */
-
-function factorial(n) { debugger
-  if (typeof(n) !== 'number' || !Number.isInteger(n)) { debugger
-      return "Ошибка, проверьте данные"; debugger
-  }
-
-  if (n >= 1) { debugger
-      return n * factorial(n - 1); debugger
-  } else { debugger
-      return 1; debugger
-  }
-
-}
-
-console.log(factorial(5)); debugger
-
+// #Task 36
