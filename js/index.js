cee65874883036658b917f3157b67ef99f4e9925
Artur Peiev <fromtheisland13@gmail.com>
2023-05-10 15:44:33 +0100

    Task-39

--- js/index.js
+++ js/index.js
@@ -1,60 +1,22 @@
 'use strict';
 
-// #Task 35
+// #Task 39
 
-//1)
+function generateList(array) {
+  let ulMenu = '<ul>';
 
-function factorial(num) { 
-
-    if (isNaN(num)) { 
-        return "Error"; 
-    }else if(num >= 1){
-      return num * factorial(num - 1); 
-    }else {
-      return 1
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      ulMenu += '<li>' + generateList(array[i]) + '</li>';
+    } else {
+      ulMenu += '<li>' + array[i] + '</li>';
     }
+  }
 
+  ulMenu += '</ul>';
+  return ulMenu;
 }
 
-console.log(factorial(5));
-console.log(factorial(6));
-console.log(factorial(7));
 
-
-//2)
-
-function degree(num, deg) { 
-  
-      if(isNaN(num) || isNaN(deg)) {
-        return 'Error'; 
-      }else if(deg === 0) { 
-        return 1; 
-      }else if( degree === 1) {
-        return num
-      }else { 
-        return  num * degree(num, deg - 1); // 2 * 2 * 2 * 2
-      }
-      
-      
-}
-
-console.log(degree(2, 4));
-console.log(degree(2, 5));
-console.log(degree(2, 0)); 
-console.log(degree(2, 6)); 
-
-
-
-//3)
-
-function sum(a, b) { 
-    
-    if(b === 0) { 
-      return a
-    }else { 
-      return sum(a + 1, b - 1)
-    }
-  
-}
-
-console.log(sum(3, 4));
+const generList = generateList([1, 2, [1.1, 1.2, 1.3], 3]);
+document.write(generList);
