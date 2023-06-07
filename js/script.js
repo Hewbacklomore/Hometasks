36faa33d14dfc2c407dca419ff915e63cd541f1c
Artur Peiev <fromtheisland13@gmail.com>
2023-06-07 20:30:22 +0100

    Task-43

--- js/script.js
+++ js/script.js
@@ -1,41 +1,34 @@
 'use strict';
 
+let user = {
+    data: {
+        a: 1,
+        b: 2,
+        c: 3,
+        d: {
+            a1: 1,
+            b1: 2,
+            c1: 3,
+            d1: {
+                a2: 3,
+                b2: 3,
+                c2: 3,
+            }
+        },
+    }
+}
 
+function closedObjectProperties(obj) {
 
-(function() {
+    
+    const descriptor = Object.getOwnPropertyNames(obj);
 
-    const form = document.querySelector('#form');
-    const inputs = form.querySelectorAll('input, select, textarea');
-    const linkSecondIndex = document.querySelector('#link');
-    const data = {};
+     for(const key in descriptor) {
+        console.log(descriptor[key]);
+    } 
+   /*  descriptor.forEach(name => {
+        console.log(obj[name]);
+    }) */
+}
 
-
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-
-
-        inputs.forEach(function({name, value}) {
-            data[name] = value;
-        });
-         
-         
-        localStorage.setItem('formData', JSON.stringify(data));
-
-        setTimeout(()=> {
-            form.style.display = 'none';
-        }, 2000)
-       
-        linkSecondIndex.removeAttribute('style');
-        linkSecondIndex.setAttribute('style', 'font-size: 40px');
-        document.body.append(linkSecondIndex);
-        
-    })
-
-    document.addEventListener('DOMContentLoaded', ()=> {
-
-        JSON.parse(localStorage.getItem('formData'));
-        
-    })
-
-}())
+console.log(closedObjectProperties(user))
