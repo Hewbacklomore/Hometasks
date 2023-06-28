5124e1a72358bb474cb5f005b0bfff276d1dac62
Artur Peiev <fromtheisland13@gmail.com>
2023-06-28 16:23:00 +0100

    Task-46

--- js/script.js
+++ js/script.js
@@ -1,34 +1,37 @@
 'use strict';
 
-let user = {
-    data: {
-        a: 1,
-        b: 2,
-        c: 3,
-        d: {
+(function () {
+
+    const user = {
+        data: {
+          a: 1,
+          b: 2,
+          c: 3,
+          d: {
             a1: 1,
             b1: 2,
             c1: 3,
             d1: {
-                a2: 3,
-                b2: 3,
-                c2: 3,
-            }
+              a2: 3,
+              b2: 3,
+              c2: 3,
+            },
+          },
         },
-    }
-}
-
-function closedObjectProperties(obj) {
-
+      };
     
-    const descriptor = Object.getOwnPropertyNames(obj);
+      function deepFreeze(obj) {
+        const propNames = Object.getOwnPropertyNames(obj);
+    
+        propNames.forEach(function (name) {
+          let property = obj[name];
+    
+          if (typeof property === "object" && property !== null)
+            deepFreeze(property);
+        });
+        return Object.freeze(obj);
+      }
+    
+      deepFreeze(user);
 
-     for(const key in descriptor) {
-        console.log(descriptor[key]);
-    } 
-   /*  descriptor.forEach(name => {
-        console.log(obj[name]);
-    }) */
-}
-
-console.log(closedObjectProperties(user))
+}())
