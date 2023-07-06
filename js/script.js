c086b6558f4286b6f1904c353d7990f0ec99f290
Artur Peiev <fromtheisland13@gmail.com>
2023-07-06 18:15:06 +0100

    Task-52

--- js/script.js
+++ js/script.js
@@ -2,27 +2,96 @@
 
  (function () {
 
-  const player1 = { id: 1, name: 'Иван', level: 10 };
-  const player2 = { id: 2, name: 'Oleg', level: 9 };
-  const player3 = { id: 3, name: 'Giorgi', level: 13 };
+    class Hamburger {
+       
+
+        constructor(size, suffering) {
+                this.size = size
+                this.suffering = suffering
+                this.objToppings = {}
+        }
+
+        static SIZE_SMALL = {
+            price: 50,
+            calories: 20
+        }
+
+        static SIZE_BIG = {
+            price: 100,
+            calories: 40
+        }
+
+        static STUFFING_CHEESE = {
+            price: 10,
+            calories: 20
+        }
+
+        static STUFFING_SALAD = {
+            price: 20,
+            calories: 5
+        }
+
+        static STUFFING_POTATO = {
+            price: 15,
+            calories: 10
+        }
+
+        static TOPPING_SAUCE = {
+            price: 15,
+            calories: 0
+        }
+
+        static TOPPING_MAYO =  {
+            price: 20,
+            calories: 5
+        } 
+
+        addTopping(item) { 
+           
+            if(Object.values(this.objToppings).length !== 0) {
+                this.objToppings = {
+                    price: this.objToppings.price + item.price,
+                    calories: this.objToppings.calories + item.calories
+                }
+                return this.objToppings
+            }
+            return this.objToppings = item
+        }
+
+        calculateCalories() {
+
+            if(typeof this.size.calories !== 'number' && typeof this.suffering.calories !== 'number' && typeof this.objToppings.calories !== 'number') throw new Error('the typeof items are not number')
+
+            const totalCalories = this.size.calories + this.suffering.calories + this.objToppings.calories
+
+            return `Calories: ${totalCalories}`
+        }
+
+        calculatePrice() {
+            if(typeof this.size.price !== 'number' && typeof this.suffering.price !== 'number' && typeof this.objToppings.price !== 'number') throw new Error('the typeof items are not number')
+
+            const totalPrice = this.size.price + this.suffering.price + this.objToppings.price
+
+            return `Price: ${totalPrice}`
+        }
+
+      }
+      
+      
+      
+const hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE); // 60 : 40
+console.log(hamburger.addTopping(Hamburger.TOPPING_MAYO)); // 80 : 45
+console.log(hamburger.calculateCalories());
+console.log(hamburger.calculatePrice());
+console.log(hamburger.addTopping(Hamburger.TOPPING_SAUCE)); // 95 : 0 = 95 : 45
+console.log(hamburger.calculateCalories());
+console.log(hamburger.calculatePrice());  
+console.log(hamburger.addTopping(Hamburger.TOPPING_SAUCE)); // 110 : 45
+console.log(hamburger.calculateCalories());
+console.log(hamburger.calculatePrice());
+console.log(hamburger.addTopping(Hamburger.TOPPING_MAYO)) // 130 : 50
+console.log(hamburger.calculateCalories());
+console.log(hamburger.calculatePrice());
 
 
-  function addPlayerToLevelSet(member) {
-
-    const mapMember = new WeakSet();
-
-    if(member.level >= 10) {
-        return (!mapMember.has(member))? mapMember.add(member) : 'there is no nothing in WeakSet'
-    }
-        return 'level of the members is too small to participiate in this game'
-
-  }
-
-  const result = addPlayerToLevelSet(player1); 
-  const result1 = addPlayerToLevelSet(player2); 
-  const result2 = addPlayerToLevelSet(player3); 
-  console.log(result);
-  console.log(result1);
-  console.log(result2);
- 
 }()) 
